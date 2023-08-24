493176d6942efd66f96324c82cc397db0e5f2e79
Grachev Denis <2717087@mail.ru>
2023-08-24 14:41:54 +0500

    dz20_1 начало

---
+++ catalog/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='Наименование')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=250, verbose_name='Наименование продукта')
+    description = models.TextField(**NULLABLE, verbose_name='Описание продукта')
+    photo = models.ImageField(upload_to='product/', **NULLABLE, verbose_name='Фото продукта')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    price = models.IntegerField(verbose_name='Цена')
+    date_make = models.DateField(verbose_name='Дата создания')
+    date_update = models.DateField(verbose_name='Дата изменения')
+
+    def __str__(self):
+        return f'{self.name} ({self.category})'
+
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
